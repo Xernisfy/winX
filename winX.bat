05977a10a7a607316e8bf5dd91118c4bc5efbc00
Arno Klein <arno.klein@epg.com>
2022-03-01 15:57:57 +0100

    fix: make the script execute from other directory

--- winX.bat
+++ winX.bat
@@ -1,7 +1,12 @@
 @ECHO OFF
 
-IF "%1" == "" TYPE help.txt & GOTO :EOF
+PUSHD "%~dp0"
+
+IF "%1" == "" TYPE help.txt & GOTO :END
 
 TASKLIST | FINDSTR vcxsrv.exe >NUL|| START /B "" vcxsrv\vcxsrv.exe -ac -wgl -compositewm -notrayicon -dpi auto -multiwindow -clipboard -primary
 SET DISPLAY=localhost:0.0
 openssh\ssh.exe -Y %1
+
+:END
+POPD
